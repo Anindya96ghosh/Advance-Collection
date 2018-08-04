5260cf262da9bcdde064334a6e3662b9901bab52
Anindya96ghosh <41475961+Anindya96ghosh@users.noreply.github.com>
2018-08-04 23:52:15 +0530

    Update Movie_Details.java

--- MovieCollection/com/collection/moviedetails/Movie_Details.java
+++ MovieCollection/com/collection/moviedetails/Movie_Details.java
@@ -6,6 +6,7 @@ public class Movie_Details {
 	private String lead_actress;
 	private String genre;
 	
+	//Parameterized Constructor
 	public Movie_Details(String mov_Name, String lead_actor, String lead_actress, String genre) {
 		super();
 		this.mov_Name = mov_Name;
@@ -14,37 +15,28 @@ public class Movie_Details {
 		this.genre = genre;
 	}
 
+	
+	//getters
 	public String getMov_Name() {
 		return mov_Name;
 	}
 	
-	public void setMov_Name(String mov_Name) {
-		this.mov_Name = mov_Name;
-	}
 	
 	public String getLead_actor() {
 		return lead_actor;
 	}
 	
-	public void setLead_actor(String lead_actor) {
-		this.lead_actor = lead_actor;
-	}
 	
 	public String getLead_actress() {
 		return lead_actress;
-	}
-	public void setLead_actress(String lead_actress) {
-		this.lead_actress = lead_actress;
 	}
 	
 	public String getGenre() {
 		return genre;
 	}
 	
-	public void setGenre(String genre) {
-		this.genre = genre;
-	}
 
+	//Overriden toString method
 	@Override
 	public String toString() {
 		return "Movie_Details [mov_Name=" + mov_Name + ", lead_actor=" + lead_actor + ", lead_actress=" + lead_actress
