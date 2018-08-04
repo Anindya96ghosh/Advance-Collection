ff0f83d4f26b8669e6d9feea3d3bce3bfd32c741
Anindya96ghosh <41475961+Anindya96ghosh@users.noreply.github.com>
2018-08-04 23:57:57 +0530

    Update Movie_DetailsList.java

--- MovieCollection/com/collection/moviedetails/Movie_DetailsList.java
+++ MovieCollection/com/collection/moviedetails/Movie_DetailsList.java
@@ -5,14 +5,17 @@ import java.util.Set;
 
 public class Movie_DetailsList {
 	
+	//creating a Set 
 	public Set<Movie_Details> movies=new LinkedHashSet<>();
 	
+	//function to add a movie to the set
 	public void add_movie(Movie_Details movie)
 	{
 		movies.add(movie);
 		System.out.println(movie);
 	}
 	
+	//function to remove a movie from the set
 	public void remove_movie(String movieName)
 	{
 		for(Movie_Details mov:movies) {
@@ -26,12 +29,14 @@ public class Movie_DetailsList {
 		movies.stream().forEach(System.out::println);
 	}
 	
+	//function to remove all movies from the set
 	public void remove_AllMovies()
 	{
 		movies.removeAll(movies);
 
 	}
 	
+	//function to find a movie from the set by its name
 	public void find_movie_By_mov_Name(String movieName) 
 	{
 
@@ -42,6 +47,8 @@ public class Movie_DetailsList {
 		
 	}
 	
+	
+	//function to find a movie from the set by its genre
 	public void find_movie_By_Genre(String genre) 
 	{
 		for(Movie_Details mov:movies) {
